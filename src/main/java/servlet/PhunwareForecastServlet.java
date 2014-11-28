7b03fe03b2c27ebd9eb9bf716fdf9c78ff8e6cb7
pgogad <pg1099@txstate.edu>
2014-11-28 11:05:35 -0800

    Adding forecast servlet

--- src/main/java/servlet/PhunwareForecastServlet.java
+++ src/main/java/servlet/PhunwareForecastServlet.java
@@ -1,8 +1,13 @@
 package servlet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,8 +30,32 @@ public class PhunwareForecastServlet extends HttpServlet
 		getForecastInfo( req, resp );
 	}
 
-	private void getForecastInfo( HttpServletRequest req, HttpServletResponse resp )
+	private void getForecastInfo( HttpServletRequest req, HttpServletResponse resp ) throws IOException
 	{
-	}
+		ServletOutputStream out = resp.getOutputStream( );
 
+		String city = req.getParameter( "city" );
+		URL url = new URL( "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&mode=json&units=metric&cnt=5");
+
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection( );
+		conn.setRequestMethod( "GET" );
+		conn.setRequestProperty( "Accept", "application/json" );
+		if ( conn.getResponseCode( ) != 200 )
+		{
+			throw new RuntimeException( "Failed : HTTP error code : " + conn.getResponseCode( ) );
+		}
+		BufferedReader br = new BufferedReader( new InputStreamReader( (conn.getInputStream( )) ) );
+		StringBuffer buff = new StringBuffer( );
+		String output;
+		while( (output = br.readLine( )) != null )
+		{
+			buff.append( output );
+			System.out.println( output );
+		}
+
+		out.write( buff.toString( ).getBytes( ) );
+		out.flush( );
+		out.close( );
+		conn.disconnect( );
+	}
 }
